e379fde0dab3fc81e09156c7fd5bf7ae794c4cf2
drago <drago@EM-LOPTAP>
2025-11-07 14:07:49 -0500

    Add project files.

---
+++ Game1.cs
@@ -0,0 +1,121 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Monogame_lesson_2___Assignment
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+
+        Rectangle window, blackSquares, whiteSquares;
+
+        Texture2D squareTexture;
+
+        int blackX;
+        int blackY;
+        int whiteX;
+        int whiteY;
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            window = new Rectangle(0, 0, 800, 800);
+            _graphics.PreferredBackBufferWidth = window.Width;
+            _graphics.PreferredBackBufferHeight = window.Height;
+            _graphics.ApplyChanges();
+
+            blackX = 0;
+            blackY = 0;
+            whiteX = 100;
+            whiteY = 0;
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // TODO: use this.Content to load your game content here
+
+            squareTexture = Content.Load<Texture2D>("square");
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // TODO: Add your drawing code here
+
+            _spriteBatch.Begin();
+
+            for (int y = 0; y <= 10; y++)
+            {
+                for (int x = 0; x <= 10; x++)
+                {
+                    blackY = 0;
+
+                    if (y == 1 || y == 3 || y == 5 || y == 7)
+                    {
+                        blackSquares = new Rectangle(((x * 200) + 100), y * 100, 100, 100);
+                    }
+                    else
+                    {
+                        blackSquares = new Rectangle(x * 200, y * 100, 100, 100);
+                    }
+
+                    _spriteBatch.Draw(squareTexture, blackSquares, Color.Black);
+
+                }
+
+                for (int x = 0; x <= 10; x++)
+                {
+                    blackY = 0;
+
+                    if (y == 2 || y == 4 || y == 6 || y == 8)
+                    {
+                        whiteSquares = new Rectangle(x * 200, ((y * 100) - 100), 100, 100);
+                    }
+                    else
+                    {
+                        whiteSquares = new Rectangle(((x * 200) + 100), ((y * 100) - 100), 100, 100);
+                    }
+
+                    _spriteBatch.Draw(squareTexture, whiteSquares, Color.White);
+                }
+            }
+
+            //for (i = 0; i <= 10; i++)
+            //{
+            //blackSquares = new Rectangle(blackX, blackY, 100, 100);
+
+            //_spriteBatch.Draw(squareTexture, blackSquares, Color.Black);
+
+            //blackY += 200;
+            //}
+            _spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,2 @@
+﻿using var game = new Monogame_lesson_2___Assignment.Game1();
+game.Run();
